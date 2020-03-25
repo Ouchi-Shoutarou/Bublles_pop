1ef99b4a1e52e4300d9b302be2ebc6286ca53021
yshr0824 <kbc19b18@stu.kawahara.ac.jp>
2020-03-25 14:52:56 +0900

    かめら

--- GameTemplate/Game/GameCamera.cpp
+++ GameTemplate/Game/GameCamera.cpp
@@ -14,14 +14,8 @@ GameCamera::~GameCamera()
 bool GameCamera::Start()
 {
 	m_spriteRender = NewGO<prefab::CSpriteRender>(0);
-	m_spriteRender->Init(L"sprite/真カーソル.dds", 1280, 720, true);
-	CQuaternion Rot2D;
-	Rot2D.SetRotationDeg(CVector3::AxisY, 180);
-	m_spriteRender->SetRotation(Rot2D);
-	m_spriteRender->SetPivot({ 0.5f,0.5f });
-	//m_position = { -20,80,0 };
-	//m_spriteRender->SetPosition(m_position);
-
+	m_spriteRender->Init(L"sprite/真カーソル.dds", 1280, 720);
+	m_spriteRender->SetPivot({0.5f, 0.5f});
 	//注視点から視点までのベクトルを設定。
 	m_toCameraPos.Set(0.0f, 50.0f, 100.0f);
 	//プレイヤーのインスタンスを探す。
@@ -39,28 +33,12 @@ bool GameCamera::Start()
 
 void GameCamera::Update()
 {
-	/*m_spriteRender->SetPosition(m_position);
-
-	m_moveSpeed.x = Pad(0).GetRStickXF() * -7.0f;
-
-	m_moveSpeed.y = Pad(0).GetRStickYF() * 7.0f;
-
-	m_position += m_moveSpeed;
-
-	m_spriteRender->SetPosition(m_position);
-
-	CVector3 position = m_position;
-	position.y += 0.0f;
-	position.z += 300.0f;
-	MainCamera().SetTarget(m_position);
-	MainCamera().SetPosition(position);
-	MainCamera().Update();*/
 
 	//カメラを更新。
 	//注視点を計算する。
 	CVector3 target = m_player->m_position;
 	//プレイヤの足元からちょっと上を注視点とする。
-	target.y += 50.0f;
+	target.y += 70.0f;
 
 	CVector3 toCameraPosOld = m_toCameraPos;
 	//パッドの入力を使ってカメラを回す。
@@ -94,9 +72,10 @@ void GameCamera::Update()
 	//視点を計算する。
 	CVector3 pos = target + m_toCameraPos;
 	//メインカメラに注視点と視点を設定する。
+	
 	MainCamera().SetTarget(target);
 	MainCamera().SetPosition(pos);
-
+	
 	//カメラの更新。
 	MainCamera().Update();
 }
--- GameTemplate/Game/Player.cpp
+++ GameTemplate/Game/Player.cpp
@@ -23,7 +23,7 @@ bool Player::Start()
 
 	m_skinModelRender = NewGO<prefab::CSkinModelRender>(0);
 	m_skinModelRender->Init(L"modelData/unityChan.cmo");
-	//m_position = { 0,0,0 };
+	m_position = { 0,0,0 };
 	m_skinModelRender->SetPosition(m_position);
 	
 	return true;
